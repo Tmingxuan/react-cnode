9745954173a006967f885d122c772aa8597aa8d2
Tmingxuan <1229052121@qq.com>
2018-05-29 15:19:05 +0800

    react-cnode

---
+++ src/ShowTopics.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class ShowTopics extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default ShowTopics
+
+const List = styled.div`
+  display: flex;
+  padding: 10px;
+  align-items: center;
+`
